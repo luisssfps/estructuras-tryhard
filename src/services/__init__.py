af6dcfb2af3a2ef0173edb31f19f956b053e18cb
Luis Felipe Polo Sanchez <luisssfps@gmail.com>
2025-11-12 16:57:24 -0500

    modificando __init__.py de services y renombrando servicio_restaurante  a su versión plural :)

---
+++ src/services/__init__.py
@@ -0,0 +1,17 @@
+from src.services.servicio_menu import ServicioDeMenu
+from src.services.servicio_mapa import ServicioDeMapa
+from src.services.servicio_clientes import ServicioDeClientes
+from src.services.servicio_domiciliarios import ServicioDeDomiciliarios
+from src.services.servicio_pedidos import ServicioDePedidos
+from src.services.servicio_restaurantes import ServicioDeRestaurantes
+from src.services.servicio_principla import SistemaPedidosDomicilio
+
+__all__ = [
+    "ServicioDeMapa",
+    "ServicioDeMenu",
+    "ServicioDeClientes",
+    "ServicioDeDomiciliarios",
+    "ServicioDePedidos",
+    "ServicioDeRestaurantes",
+    "SistemaPedidosDomicilio",
+]
